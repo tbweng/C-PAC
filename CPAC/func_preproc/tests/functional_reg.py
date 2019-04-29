7d205f95367da00b0a3d0f6bc38850e6699e5da2
Foulon <cf27246@dhcp-10-157-81-43.dellmedschool.utexas.edu>
2019-04-29 12:16:53 -0500

    Adding the possibility to use ants instead of FLIRT for the template generation

--- CPAC/func_preproc/tests/functional_reg.py
+++ CPAC/func_preproc/tests/functional_reg.py
@@ -5,11 +5,49 @@ import ntpath
 import numpy as np
 
 import nibabel as nib
-import numpy as np
 import nipype.pipeline.engine as pe
 import nipype.interfaces.utility as util
 import nipype.interfaces.fsl as fsl
 from nipype import MapNode
+
+
+def read_ants_mat(ants_mat_file):
+    if not os.path.exists(ants_mat_file):
+        raise ValueError(str(ants_mat_file) + " does not exist.")
+
+    with open(ants_mat_file) as f:
+        for line in f:
+            tmp = line.split(':')
+            if tmp[0] == 'Parameters':
+                oth_transform = np.reshape(
+                    np.fromstring(tmp[1], float, sep=' '), (-1, 3))
+            if tmp[0] == 'FixedParameters':
+                translation = np.fromstring(tmp[1], float, sep=' ')
+    return translation, oth_transform
+
+
+def read_flirt_mat(flirt_mat):
+    if isinstance(flirt_mat, np.ndarray):
+        mat = flirt_mat
+    elif isinstance(flirt_mat, str):
+        if os.path.exists(flirt_mat):
+            mat = np.loadtxt(flirt_mat)
+        else:
+            raise IOError("ERROR norm_transformation: " + flirt_mat +
+                          " file does not exist")
+    else:
+        raise TypeError("ERROR norm_transformation: flirt_mat should be" +
+                        " either a str or a numpy.ndarray matrix")
+
+    if mat.shape != (4, 4):
+        raise ValueError("ERROR norm_transformation: the matrix should be 4x4")
+
+    # Translation vector
+    translation = mat[0:3, 3]
+    # 3x3 matrice of rotation, scaling and skewing
+    oth_transform = mat[0:3, 0:3]
+
+    return translation, oth_transform
 
 
 def simple_stats(image, method='mean', axis=3):
@@ -52,6 +90,31 @@ def simple_stats(image, method='mean', axis=3):
     out_data = getattr(np, method)(data, axis)
 
     return nib.nifti1.Nifti1Image(out_data, img.affine)
+
+
+def average_images(images_list, method='median', axis=3):
+    """
+    Average each image of images_list with the given method and in the given
+    axis into a list of lower dimension images. (Typically 4D to 3D)
+    Parameters
+    ----------
+    images_list: list of str
+        list of images (typically 4D functional images) paths
+    method: {'mean', 'median', 'std', 'average' ...}, optional
+        method used to average the 4D image.
+        most of the functions from numpy having only one array in mandatory
+        parameter and an axis option will work.
+        default is 'mean'
+    axis: int, optional
+        default is 3 (for the temporal resolution of fMRI for example)
+
+    Returns
+    -------
+
+    """
+    img_list = [simple_stats(
+        img, method=method, axis=axis) for img in images_list]
+    return img_list
 
 
 def init_robust_template(wf_name='init_robust'):
@@ -107,7 +170,7 @@ def create_temporary_template(img_list, out_path, avg_method='median'):
     tmp_template: Nifti1Image
     """
     if not img_list:
-        print('ERROR create_temporary_template: image list is empty')
+        raise ValueError('ERROR create_temporary_template: image list is empty')
 
     if len(img_list) == 1:
         return img_list[0]
@@ -150,7 +213,7 @@ def register_img_list(img_list, ref_img, output_folder, dof=12,
         outputs.out_matrix_file will contain the transformation matrices
     """
     if not img_list:
-        print('ERROR create_temporary_template: image list is empty')
+        raise ValueError('ERROR register_img_list: image list is empty')
 
     # output_folder = os.getcwd()
 
@@ -187,20 +250,81 @@ def register_img_list(img_list, ref_img, output_folder, dof=12,
     return multiple_linear_reg
 
 
+def norm_transformations(translation, oth_transform):
+    tr_norm = np.linalg.norm(translation)
+    affine_norm = np.linalg.norm(oth_transform - np.identity(3), 'fro')
+    return pow(tr_norm, 2) + pow(affine_norm, 2)
+
+
 def norm_transformation(flirt_mat):
+    """
+    Calculate the squared norm of the translation + squared Frobenium norm
+    of the difference between other affine transformations and the identity
+    from an fsl FLIRT transformation matrix
+    Parameters
+    ----------
+    flirt_mat: str or numpy.ndarray
+        Either the path to an fsl flirt matrix or an flirt matrix already
+        imported.
+
+    Returns
+    -------
+        numpy.float64
+            squared norm of the translation + squared Frobenium norm of the
+            difference between other affine transformations and the identity
+    """
+    if isinstance(flirt_mat, np.ndarray):
+        mat = flirt_mat
+    elif isinstance(flirt_mat, str):
+        if os.path.exists(flirt_mat):
+            mat = np.loadtxt(flirt_mat)
+        else:
+            raise IOError("ERROR norm_transformation: " + flirt_mat +
+                          " file does not exist")
+    else:
+        raise TypeError("ERROR norm_transformation: flirt_mat should be" +
+                        " either a str or a numpy.ndarray matrix")
+
+    if mat.shape != (4, 4):
+        raise ValueError("ERROR norm_transformation: the matrix should be 4x4")
+
     # Translation vector
-    translation = flirt_mat[0:3, 3]
+    translation = mat[0:3, 3]
     # 3x3 matrice of rotation, scaling and skewing
-    oth_affine_transform = flirt_mat[0:3, 0:3]
+    oth_affine_transform = mat[0:3, 0:3]
     tr_norm = np.linalg.norm(translation)
     affine_norm = np.linalg.norm(oth_affine_transform - np.identity(3), 'fro')
     return pow(tr_norm, 2) + pow(affine_norm, 2)
 
 
-def template_convergence(mat_file,
+def template_convergence(mat_file, mat_type='flirt',
                          convergence_threshold=np.finfo(np.float64).eps):
-    mat1 = np.loadtxt(mat_file)
-    distance = norm_transformation(mat1)
+    """
+
+    Parameters
+    ----------
+    mat_file: str
+        path to an fsl flirt matrix
+    mat_type: str
+        'flirt'(default), 'ants'
+        The type of matrix used to represent the transformations
+    convergence_threshold: float
+        (numpy.finfo(np.float64).eps (default)) threshold for the convergence
+        The threshold is how different from no transformation is the
+        transformation matrix.
+
+    Returns
+    -------
+
+    """
+    if mat_type == 'flirt':
+        translation, oth_transform = read_flirt_mat(mat_file)
+    elif mat_type == 'ants':
+        translation, oth_transform = read_ants_mat(mat_file)
+    else:
+        raise ValueError("ERROR template_convergence: this matrix type does " +
+                         "not exist")
+    distance = norm_transformations(translation, oth_transform)
 
     return abs(distance) <= convergence_threshold
 
@@ -215,8 +339,10 @@ def template_creation_loop(img_list, output_folder,
     ----------
     img_list: list of str
         list of images paths
-    output_folder
+    output_folder: str
+        path to the output folder (the folder must already exist)
     init_reg: nipype.MapNode
+        (default None so no initial registration is performed)
         the output of the function register_img_list with another reference
         Reuter et al. 2012 (NeuroImage) section "Improved template estimation"
         doi:10.1016/j.neuroimage.2012.02.084 recommend to use a ramdomly
@@ -246,20 +372,28 @@ def template_creation_loop(img_list, output_folder,
     if not img_list:
         print('ERROR create_temporary_template: image list is empty')
 
-    image_list = img_list
-    converged = False
+    if init_reg is not None:
+        image_list = init_reg.inputs.out_file
+        mat_list = init_reg.inputs.out_matrix_file
+        convergence_list = [template_convergence(
+            mat, 'flirt', convergence_threshold) for mat in mat_list]
+        converged = all(convergence_list)
+    else:
+        image_list = img_list
+        converged = False
+
     tmp_template = os.path.join(output_folder, 'tmp_template.nii.gz')
 
     while not converged:
         tmp_template = create_temporary_template(image_list,
-                                                 tmp_template,
-                                                 avg_method='median')
+                                                 out_path=tmp_template,
+                                                 avg_method=avg_method)
         reg_list_node = register_img_list(image_list,
-                                          tmp_template,
-                                          output_folder,
-                                          dof=12,
-                                          interp='trilinear',
-                                          cost='corratio')
+                                          ref_img=tmp_template,
+                                          output_folder=output_folder,
+                                          dof=dof,
+                                          interp=interp,
+                                          cost=cost)
         reg_list_node.run()
 
         image_list = reg_list_node.inputs.out_file
